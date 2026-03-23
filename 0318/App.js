260720825004b43c53fe8995ae921f2b9369f786
kaisa12257 <ljy203060@gmail.com>
2026-03-23 20:30:30 +0900

    Update App.js

--- 0318/App.js
+++ 0318/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style={{ backgroundColor: '#282c34', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
+        <img src={logo} className="App-logo" alt="logo" style={{ height: '40vmin', pointerEvents: 'none' }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,6 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ color: '#61dafb' }}
         >
           Learn React
         </a>
